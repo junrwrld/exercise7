8697c5b221c904e7ec8de8d8fb94e71296f33d4e
João Junior <batistasousajoao@gmail.com>
2023-07-28 18:35:29 -0300

    Adicionar arquivos de projeto.

---
+++ exercise7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercise7
+{
+    public class _exercise7
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua senha");
+            int senha = int.Parse(Console.ReadLine());  
+
+            while ( senha != 2002)
+            {
+                Console.WriteLine("Senha incorreta! Digite novamente");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido! Seja bem vindo");
+        }
+    }
+    
+}
